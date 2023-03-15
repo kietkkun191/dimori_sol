c2bf3fece0f8729cf1d49177c07fc75ffcba1794
kietkkun191 <kietnguyen.work@gmail.com>
2023-03-15 07:11:10 +0700

    first commit

---
+++ frontend/components/about.js
@@ -0,0 +1,20 @@
+
+function About() {
+    return (
+        // <div className="px-20 pb-10 flex justify-between items-center bg-white">
+        // <div className="justify-item-center">
+        <div className="px-20 pt-10 pb-20 flex justify-between items-center bg-gray-800">
+            <div id="about" className="flex-1 flex xl:justify-center items-center px-6 transition-all duration-300">
+                <blockquote>
+                    <p className="text-xl text-center text-white font-medium">
+                    Dimori is a Web 3.0 homestay booking platform on Solana. Its vision is to become the first community-owned platform in Vietnam that connects aligned value hosts and guests to book homestays for local experience while earning real-world rewards.
+                    </p>
+                </blockquote>
+            </div>
+        </div>
+        // </div>
+        
+    )
+}
+
+export default About
---
+++ frontend/constants/index.js
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+// export const AIRBNB_PROGRAM_PUBKEY = new PublicKey("J5yzCZrNZJR6K5FUwwZ2SzjTvg8DzomcYqcaZJm27Y6v");
+export const PROGRAM_PUBKEY = new PublicKey("BiqcY2D2R7RK3CC2W24vcJRJJD6KLViXmKHjzLMq5vQN");
+
